4ec3623ef3b1e1ff1af7f6e8dfd57b2a015c277c
joannewei <joannewei0921@gmail.com>
2022-05-05 01:02:50 +0800

    update: DB connection

--- connections/index.js
+++ connections/index.js
@@ -4,10 +4,10 @@ var dotenv = require('dotenv');
 dotenv.config({ path: './config.env' })
 
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
-// mongoose.connect(DB)
-//   .then(()=>console.log('資料庫連線成功'))
-//   .catch (err=> console.log(err))
+mongoose.connect(DB)
+  .then(()=>console.log('資料庫連線成功'))
+  .catch (err=> console.log(err))
 
-mongoose.connect("mongodb://localhost:27017/homework")
-  .then(() => console.log('資料庫連線成功'))
-  .catch(err => console.log(err))
+// mongoose.connect("mongodb://localhost:27017/homework")
+//   .then(() => console.log('資料庫連線成功'))
+//   .catch(err => console.log(err))
